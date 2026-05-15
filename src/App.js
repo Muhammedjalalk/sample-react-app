b9afb7e03d4569c3afb5519b306434dddc4e75c3
muhammed jalal <muhammedjalal928@gmail.com>
2026-05-15 21:44:12 +0530

    Updated App.js and Counter_App.jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import LoginForm from './pages/Loginform';
+// import LoginForm from './pages/Loginform';
+
+import CountApp from './pages/Counter_App';
 
 
 function App() {
   return (
     <div>
-      <LoginForm/>
+      {/* <LoginForm/> */}
+      <CountApp/>
     </div>
   );
 }
---
+++ src/pages/Counter_App.jsx
@@ -0,0 +1,58 @@
+import{useState} from "react";
+
+function CountApp(){
+
+    const[count,setCount]=useState(0);
+    const increase = () =>{
+        setCount(count +1);
+    };
+ const decrease = () =>{
+      if(count>0){
+        setCount(count-1);
+    }
+};
+
+
+const Reset=() =>{
+    setCount(0);
+};
+return(
+    <div 
+    style={{
+        display:"flex",
+        justifyContent:"center",
+        alignItems:"center",
+        height:"100vh",
+        backgroundColor:"#f2f2f2"
+    }}>
+
+        <div
+        style={{
+            padding:"30px",
+            backgroundColor:"white",
+            borderRadius:"10px",
+            boxShadow:"0px 0px 10px gray",
+            width:"300px"
+        }}>
+        <h1>Counter App</h1>
+
+         <h2> {count}</h2>
+        <button onClick={increase}>
+            Increase
+        </button>
+
+         <button onClick={decrease}
+           style={{marginLeft:"10px"}}>
+            Decrease
+        </button>
+
+          <button onClick={Reset}
+           style={{marginLeft:"10px"}}>
+            Reset
+        </button>
+
+    </div>
+    </div>
+)
+};
+export default CountApp;
